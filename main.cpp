2b763fa3d928c517708eb85752307cbca0afd8df
ferris <jake@fusetools.com>
2015-10-25 14:41:24 -0700

    Preliminary theming test works
    now to do it properly

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
     if (argc > 1)
         apu.SetSong(argv[1]);
 
+    a.setStyleSheet("QWidget { color: #ffffff; background-color: #000000; } QPushButton { background-color: #0a0a0a; border: none; } QPushButton:hover { background-color: #1a1a1a; color: #00c6ff; } QPushButton:pressed { background-color: #c6076c; color: #000000; }");
+
     MainWindow w(&apu);
     w.show();
 
